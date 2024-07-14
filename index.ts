24caa01a080d55d0c5cd7b2818f02c7971a7a338
danju4rizzl <danjumashiwaju@gmail.com>
2024-07-14 19:24:45 +0200

    added docs

--- index.ts
+++ index.ts
@@ -7,6 +7,7 @@
  * A very simple function to add numbers.
  * @param a 1st number you want to add
  * @param b 2nd number you want to add
+ * @returns the sum of the two numbers
  */
 
-export const sum = (a: number, b: number) => a + b
+export const sum = (a: number, b: number): number => a + b
