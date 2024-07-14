42523356fa95c998c4d58293887255c73e3f459c
danju4rizzl <danjumashiwaju@gmail.com>
2024-07-14 19:05:07 +0200

    init commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * A module function to calculate numbers.
+ * @module
+ */
+
+/**
+ * A very simple function to add numbers.
+ * @param a 1st number you want to add
+ * @param b 2nd number you want to add
+ */
+
+export const sum = (a: number, b: number) => a + b
